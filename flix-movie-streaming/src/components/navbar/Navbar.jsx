dbc3564f6ba32034e652396fe80a4a44a581b163
CodeJunkie97 <ianmanners26@outlook.com>
2023-05-12 14:23:23 -0500

    Finishing up Navbar Next I will begin work on featured movie component

--- flix-movie-streaming/src/components/navbar/Navbar.jsx
+++ flix-movie-streaming/src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import './navbar.scss'
 import logo from './flix.png'
+import Search from '@mui/icons-material/Search'
+import Notifications from '@mui/icons-material/Notifications'
+import ArrowDropDown from '@mui/icons-material/ArrowDropDown'
+import { useState } from 'react'
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () =>{
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => {window.onscroll = null};
+  }
+  
   return (
-    <div className = "navbar">
+    <div className = {isScrolled ? "navbar scrolled" : "navbar"}>
         <div className="container">
             <div className="left">
                 <img
@@ -15,8 +25,19 @@ const Navbar = () => {
             <span>New and Popular</span>
             <span>My List</span>
             </div>
-            <div className="right"></div>
-    
+            <div className="right">
+            <Search className = "icon" />
+            <span>KID</span>
+            <Notifications className = "icon" />
+            <img src = 'https://th.bing.com/th/id/OIP.4oBn_yCg_ZvGg2ENIW8s9gHaDt?pid=ImgDet&rs=1' alt = ''/>
+           <div className="profile">
+            <ArrowDropDown className = "icon" />
+              <div className = "options">
+                <span>Settings</span>
+                <span>Logout</span>
+              </div>
+            </div>
+            </div>
         </div>
     </div>
   )
