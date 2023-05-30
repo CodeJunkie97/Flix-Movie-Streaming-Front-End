f40e1ce5cdee8d32f6d30044e03afe5d9e8cbd68
Ian Manners <IMM17@pitt.edu>
2023-05-29 22:19:21 -0500

    Finish Login Page

--- flix-movie-streaming/src/App.jsx
+++ flix-movie-streaming/src/App.jsx
@@ -2,8 +2,9 @@ import Home from './components/pages/home/Home'
 import './App.scss'
 import Watch from './components/pages/watch/Watch';
 import Register from './components/pages/register/Register';
+import Login from './components/pages/login/Login';
 const App = () => {
-  return <Register/>;
+  return <Login/>;
 };
 
 export default App;
